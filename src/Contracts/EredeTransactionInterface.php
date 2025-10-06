794bf580be659be7ef731b156be118be80e09ffb
Lucas Silva <lcs13761@gmail.com>
2025-10-06 10:35:54 -0300

    changes for refund

--- src/Contracts/EredeTransactionInterface.php
+++ src/Contracts/EredeTransactionInterface.php
@@ -10,8 +10,12 @@ interface EredeTransactionInterface
 {
     public function createTransaction(PaymentRequestDTO $transactionData): PaymentRequestDTO;
     public function captureTransaction(string $transactionId, int $amount = null): PaymentRequestDTO;
-    public function cancelTransaction(string $transactionId, int $amount = null): PaymentRequestDTO;
+    public function cancelTransaction(string $transactionId, array $data): PaymentRequestDTO;
     public function getTransaction(string $transactionId): PaymentRequestDTO;
     public function getTransactionByReference(string $reference): PaymentRequestDTO;
+
+    public function refundTid(string $transactionId): PaymentRequestDTO;
+
+    public function refundId(string $transactionId, string $refundId): PaymentRequestDTO;
 
 }
--- src/Services/EredeTransaction.php
+++ src/Services/EredeTransaction.php
@@ -56,23 +56,6 @@ readonly class EredeTransaction implements EredeTransactionInterface
      * @throws TransactionException
      * @throws Exception
      */
-    public function cancelTransaction(string $transactionId, ?int $amount = null): PaymentRequestDTO
-    {
-        $data = [];
-
-        if ($amount !== null) $data['amount'] = $amount;
-
-        $response = $this->httpClient->request(method: HttpMethod::DELETE, endpoint: $transactionId, data: $data);
-
-        if (!$response->isSuccessful()) throw TransactionException::invalidTransaction('Falha ao cancelar transação', $response->data);
-
-        return (new PaymentRequestDTO)->fromArray($response->data);
-    }
-
-    /**
-     * @throws TransactionException
-     * @throws Exception
-     */
     public function getTransaction(string $transactionId): PaymentRequestDTO
     {
         $response = $this->httpClient->request(method: HttpMethod::GET, endpoint: $transactionId);
@@ -91,6 +74,48 @@ readonly class EredeTransaction implements EredeTransactionInterface
         $response = $this->httpClient->request(method: HttpMethod::GET, endpoint: '', data: ['reference' => $reference]);
 
         if (!$response->isSuccessful()) throw TransactionException::transactionNotFound($reference);
+
+        return (new PaymentRequestDTO)->fromArray($response->data);
+    }
+
+    /**
+     * @param string $transactionId
+     * @return PaymentRequestDTO
+     * @throws TransactionException
+     */
+    public function refundTid(string $transactionId): PaymentRequestDTO
+    {
+        $response = $this->httpClient->request(method: HttpMethod::GET, endpoint: $transactionId . "/refunds");
+
+        if (!$response->isSuccessful()) throw TransactionException::transactionNotFound($transactionId);
+
+        return (new PaymentRequestDTO)->fromArray($response->data);
+    }
+
+    /**
+     * @param string $transactionId
+     * @param string $refundId
+     * @return PaymentRequestDTO
+     * @throws TransactionException
+     */
+    public function refundId(string $transactionId, string $refundId): PaymentRequestDTO
+    {
+        $response = $this->httpClient->request(method: HttpMethod::GET, endpoint: $transactionId . "/refunds/" . $refundId);
+
+        if (!$response->isSuccessful()) throw TransactionException::transactionNotFound($transactionId);
+
+        return (new PaymentRequestDTO)->fromArray($response->data);
+    }
+
+    /**
+     * @throws TransactionException
+     * @throws Exception
+     */
+    public function cancelTransaction(string $transactionId, array $data = []): PaymentRequestDTO
+    {
+        $response = $this->httpClient->request(method: HttpMethod::POST, endpoint: $transactionId . "/refunds", data: $data);
+
+        if (!$response->isSuccessful()) throw TransactionException::invalidTransaction('Falha ao cancelar transação', $response->data);
 
         return (new PaymentRequestDTO)->fromArray($response->data);
     }
